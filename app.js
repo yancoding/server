770330699615ed85efa8c81ad8c21731add7ef8f
yan <yancoding@qq.com>
2020-07-24 00:37:05 +0800

    引入websocket

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Koa = require('koa')
 const cors = require('@koa/cors')
 const bodyParser = require('koa-bodyparser')
 const KoaStatic = require('koa-static')
+require('./websocket.js')
 require('dotenv').config()
 
 const {
