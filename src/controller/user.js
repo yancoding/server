d078d3d7661b35ef31dae387a5d824391e5b0f71
yan <yancoding@qq.com>
2021-08-27 21:38:57 +0800

    是否登录

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,9 +1,19 @@
 class UserController {
   // 用户信息
   static async user(ctx, next) {
+    const userInfo = ctx.state.userinfo
+    if (!userInfo)  {
+      ctx.body = {
+        success: false,
+        data: null,
+        msg: '未登录',
+      }
+      return
+    }
+
     ctx.body = {
       success: true,
-      data: ctx.state.userinfo,
+      data: userInfo,
       msg: '获取成功',
     }
   }
